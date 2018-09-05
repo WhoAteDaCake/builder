bf177fe641016ebb5c0a699ea3d27bb841947bbf
whoatedacake <doitsimplefy@gmail.com>
2018-09-05 22:29:04 +0100

    update gitignore

--- examples/document-viewer/run.bash
+++ examples/document-viewer/run.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git clone git@github.com:SpotlightData/nanowire-extensions.git
+mv -r nanowire-extensions repo
 cp .builder.js ./nanowire-extensions
 cd nanowire-extensions
 yarn packages
