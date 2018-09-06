499a06f7f40604468777b5f1e2a50d42c5e6411b
whoatedacake <doitsimplefy@gmail.com>
2018-09-06 20:28:38 +0100

    fix react input file

--- examples/nanowire-extensions/run.bash
+++ examples/nanowire-extensions/run.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# rm -rf repo
-# git clone git@github.com:SpotlightData/nanowire-extensions.git
-# mv nanowire-extensions repo
+rm -rf repo
+git clone git@github.com:SpotlightData/nanowire-extensions.git
+mv nanowire-extensions repo
 cp .builder.js ./repo || true
 cd repo
-# yarn packages
+yarn packages
 ../../../bin/builder.js bundle
+../../../bin/builder.js library
