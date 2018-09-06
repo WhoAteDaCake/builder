ee07ffd42ad867af203a55d26d69d759eb00e127
whoatedacake <doitsimplefy@gmail.com>
2018-09-06 20:28:56 +0100

    update named exports

--- src/defaults.js
+++ src/defaults.js
@@ -35,7 +35,7 @@ const build = {
     include: 'node_modules/**',
     ignore: [],
     namedExports: {
-      'node_modules/react/react.js': ['createElement', 'PureComponent', 'Component'],
+      'node_modules/react/index.js': ['createElement', 'PureComponent', 'Component'],
       'node_modules/react-dom/index.js': ['findDOMNode', 'createPortal', 'render'],
     },
   },
