38853d1005828b8555b123fcb56d2fc959ea07e9
whoatedacake <doitsimplefy@gmail.com>
2018-09-12 21:30:48 +0100

    add initial configuration to start

--- src/pipeline/run.js
+++ src/pipeline/run.js
@@ -1,5 +1,6 @@
 const R = require('ramda');
 const checkForDebug = require('../helpers/checkForDebug');
+const configure = require('./configure');
 
 function run(scripts, initial = {}) {
   return scripts.reduce((value, fn) => fn(value), initial);
@@ -10,7 +11,7 @@ const runC = R.curryN(2, run);
 // Because we want to only show warning once
 function start(scripts, initial) {
   checkForDebug();
-  return run(scripts, initial);
+  return run([configure()].concat(scripts), initial);
 }
 
 module.exports = { run, start, runC };
