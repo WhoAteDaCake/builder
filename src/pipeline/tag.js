ae0eba1041149fe2a29289b18e59e7e9da432630
whoatedacake <doitsimplefy@gmail.com>
2018-09-12 21:24:36 +0100

    tag functionality

--- src/pipeline/tag.js
+++ src/pipeline/tag.js
@@ -2,11 +2,28 @@ const fs = require('fs-extra');
 const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const R = require('ramda');
+const debug = require('../helpers/debug')('pipeline:tag');
 const { runC } = require('./run');
 const getMeta = require('./getMeta');
 const configuration = require('./internal/configuration');
 const { PipelineError } = require('../helpers/error');
 
+const commands = {
+  gitNotCommited: 'git status --untracked-files=no --porcelain',
+  gitBranch: "git branch | grep \\* | cut -d ' ' -f2",
+};
+
+const logStd = ({ stdout, stderr }) => {
+  if (stdout.length !== 0) {
+    debug(stdout);
+  }
+  if (stderr.length !== 0) {
+    debug(stderr);
+  }
+  return { stdout, stderr };
+};
+const notEmptyStdout = resp => resp.stdout.length !== 0;
 const versionFromArgv = R.pathOr(undefined, ['argv', '3']);
 
 function tagCore(version) {
@@ -14,11 +31,14 @@ function tagCore(version) {
     const finalVersion = version ? version : versionFromArgv(config);
     const packageVersion = R.pathOr(undefined, ['meta', 'pkg', 'version'], config);
     const runner = exec;
-    return configuration.add({
-      finalVersion,
-      packageVersion,
-      runner,
-    });
+    return configuration.add(
+      {
+        finalVersion,
+        packageVersion,
+        runner,
+      },
+      config
+    );
   };
 }
 
@@ -26,17 +46,29 @@ function tagRun(config) {
   const { finalVersion, packageVersion, runner } = configuration.get(config);
   const action = R.propOr(Promise.resolve(), 'action', config);
   if (!finalVersion) {
-    throw new PipelineError(`Tag version was not defined`);
+    throw new PipelineError('Tag version was not defined');
   }
   if (!packageVersion) {
-    throw new PipelineError(`Version was not found in package.json`);
+    throw new PipelineError('Version was not found in package.json');
   }
   if (packageVersion !== finalVersion) {
-    throw new PipelineError(`Tag version does not match version in package.json`);
+    throw new PipelineError('Tag version does not match version in package.json');
   }
 
   async function runAsync() {
-    // const hasChanges = git diff-index --quiet HEAD
+    const hasUncommited = await runner(commands.gitNotCommited).then(notEmptyStdout);
+    if (hasUncommited) {
+      throw new PipelineError('Please commit the changes before tagging');
+    }
+    const branch = await runner(commands.gitBranch).then(resp => resp.stdout.replace('\n', ''));
+    const hasUnPushed = await runner(`git log --oneline origin/${branch}..${branch}`).then(
+      notEmptyStdout
+    );
+    if (hasUnPushed) {
+      throw new PipelineError('Please push the changes before tagging');
+    }
+    await runner(`git tag -a v${finalVersion} -m v${finalVersion}`).then(logStd);
+    await runner(`git push origin v${finalVersion}`).then(logStd);
   }
 
   return R.merge(config, {
