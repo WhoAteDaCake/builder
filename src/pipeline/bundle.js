0d0ddb62df3d49349cfb1daecba3e8243c7a8e13
WhoAteDaCake <doitsimplefy@gmail.com>
2018-12-10 21:55:11 +0000

    allow runs to crash and exit with error code

--- src/pipeline/bundle.js
+++ src/pipeline/bundle.js
@@ -6,31 +6,32 @@ const plugins = require('../helpers/plugins');
 const debug = require('../helpers/debug')('pipeline:bundle');
 
 function bundle(local) {
-  return shared => {
-    const config = R.mergeDeepRight(shared, local);
-    const { rollup, babel } = config.build;
-    const action = R.propOr(Promise.resolve(), 'action', config);
-    const files = R.mergeDeepRight({
-      input: 'src/index.js',
-      output: 'build/index.js',
-    })(config.files);
-    const output = { file: files.output, format: rollup.format };
-    const outputDir = path.dirname(output.file);
+	return (shared) => {
+		const config = R.mergeDeepRight(shared, local);
+		const { rollup, babel } = config.build;
+		const action = R.propOr(Promise.resolve(), 'action', config);
+		const files = R.mergeDeepRight({
+			input: 'src/index.js',
+			output: 'build/index.js'
+		})(config.files);
+		const output = { file: files.output, format: rollup.format };
+		const outputDir = path.dirname(output.file);
 
-    function runAsync() {
-      return runRollup(
-        {
-          ...rollup.extra,
-          input: files.input,
-          plugins: plugins(rollup, babel),
-        },
-        output
-      ).catch(e => debug(e));
-    }
-    return R.merge(config, {
-      action: action.then(() => removeDir(config.meta.home, outputDir)).then(runAsync),
-    });
-  };
+		const run = () =>
+			removeDir(config.meta.home, outputDir).then(() =>
+				runRollup(
+					{
+						...rollup.extra,
+						input: files.input,
+						plugins: plugins(rollup, babel)
+					},
+					output
+				)
+			);
+		// Not using merge here speeds it up
+		config.action = action.then(run);
+		return config;
+	};
 }
 
 module.exports = bundle;
