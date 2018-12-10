4be5dac260075e4a5de6c09c5ace2b1aa1c4aad2
WhoAteDaCake <doitsimplefy@gmail.com>
2018-12-10 21:57:47 +0000

    update rollup config

--- src/config/rollup.js
+++ src/config/rollup.js
@@ -1,27 +1,26 @@
 const debug = require('../helpers/debug')('rollup');
 
 function external(id) {
-  return !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
+	return !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/');
 }
 
 function onwarn(message) {
-  // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
-  // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
-  if (message.code === 'THIS_IS_UNDEFINED') {
-    return;
-  }
-  // debug('%O', message);
-  debug(message.message);
+	// Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+	// https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+	if (message.code === 'THIS_IS_UNDEFINED') {
+		return;
+	}
+	debug(message.message);
 }
 
 module.exports = {
-  format: 'cjs',
-  resolve: require('./resolve'),
-  commonjs: require('./commonjs'),
-  json: require('./json.js'),
-  extra: {
-    external,
-    onwarn,
-    treeshake: true,
-  },
+	format: 'cjs',
+	resolve: require('./resolve'),
+	commonjs: require('./commonjs'),
+	json: require('./json.js'),
+	extra: {
+		external,
+		onwarn,
+		treeshake: true
+	}
 };
